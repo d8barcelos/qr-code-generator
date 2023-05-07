610c2f05cac9945db60567987f8a8c2d01c3d9de
Diogo <d8barcelos@gmail.com>
2023-05-07 17:28:42 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
-import React from 'react'
-import './App.css'
+import React, { useState, useEffect, useRef } from 'react';
+import QRCode from 'react-qr-code';
+import QRCodeLib from 'qrcode';
+import './App.css';
 
 const App = () => {
+  const [link, setLink] = useState('');
+  const [qrCodeBlob, setQRCodeBlob] = useState(null);
+  const qrCodeLinkRef = useRef(null);
+
+  useEffect(() => {
+    if (qrCodeBlob !== null) {
+      qrCodeLinkRef.current.href = URL.createObjectURL(qrCodeBlob);
+    }
+  }, [qrCodeBlob]);
+
+  const handleGenerate = (linkUrl) => {
+    QRCodeLib.toCanvas(
+      document.createElement('canvas'),
+      linkUrl,
+      {
+        width: 600,
+        margin: 3,
+      },
+      (error, canvas) => {
+        if (error) {
+          console.error(error);
+        } else {
+          canvas.toBlob((blob) => {
+            setQRCodeBlob(blob);
+          }, 'image/png');
+        }
+      }
+    );
+  };
+
+  const handleQrcode = (e) => {
+    setLink(e.target.value);
+    handleGenerate(e.target.value);
+  };
+
   return (
-    <div>App</div>
-  )
-}
+    <div className='container'>
+      <QRCode value={link} />
+      <input
+        className='input'
+        placeholder='Enter your link...'
+        value={link}
+        onChange={handleQrcode}
+      />
+      <a
+        href='#'
+        ref={qrCodeLinkRef}
+        download='qrcode.png'
+      >
+        Download QR Code
+      </a>
+    </div>
+  );
+};
 
 export default App;
